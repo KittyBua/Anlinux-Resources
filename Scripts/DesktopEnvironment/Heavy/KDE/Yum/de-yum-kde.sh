e6dae7749574f672514685145dff1a85a015c9e1
exalab999 <exalabdevelopers@gmail.com>
2026-07-12 16:09:45 +0800

    Update de-yum-kde.sh

--- Scripts/DesktopEnvironment/Heavy/KDE/Yum/de-yum-kde.sh
+++ Scripts/DesktopEnvironment/Heavy/KDE/Yum/de-yum-kde.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Get the necessary components
-yum groupinstall kde-desktop -y
+yum group install kde-desktop -y
 yum install tigervnc-server dbus-x11 -y
 
 #Setup the necessary files
