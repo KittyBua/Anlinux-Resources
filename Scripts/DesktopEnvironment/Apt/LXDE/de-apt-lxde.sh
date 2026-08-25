16554b54d19713710f49f3c2ccf98f7732d1ad2e
exalab999 <exalabdevelopers@gmail.com>
2026-08-26 04:45:01 +0800

    update

--- Scripts/DesktopEnvironment/Apt/LXDE/de-apt-lxde.sh
+++ Scripts/DesktopEnvironment/Apt/LXDE/de-apt-lxde.sh
@@ -15,7 +15,7 @@ wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/D
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Xfce4/vncserver-stop --no-check-certificate -P /usr/local/bin/
 
 chmod +x ~/.vnc/xstartup
-chmod +x ~/.config/tigervnc/
+chmod +x ~/.config/tigervnc/xstartup
 chmod +x /usr/local/bin/vncserver-start
 chmod +x /usr/local/bin/vncserver-stop
 
